25cd759554d14a3f0229b42c41a23fa5f21bf880
Lisvindanu <Lisvindanu015@gmail.com>
2025-05-08 15:21:04 +0700

    Fixsasi tampilan jawaban si ai

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f54ea05a0a77abea2620755e3faa9c25e1d3e107',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f54ea05a0a77abea2620755e3faa9c25e1d3e107',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
